89f974ae2fa7c4de59c8fac3d4303753faa871dc
TgSBot <falleytrd88@mail.ru>
2023-06-27 23:47:23 +0500

    Minor edits

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-// import HomePage from '../modules/HomePage/HomePage.jsx';
+import HomePage from '../modules/HomePage/HomePage.jsx';
 import Stock from '../modules/Stock/Stock.jsx';
-// import StoryGame from '../modules/StoryGame/StoryGame.jsx';
+import StoryGame from '../modules/StoryGame/StoryGame.jsx';
 
 const MainPage = () => {
 	return (
 		<div>
-			{/* <HomePage />
-			<StoryGame /> */}
+			<HomePage />
+			<StoryGame />
 			<Stock />
 		</div>
 	);
