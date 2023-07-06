9912bb3bfd13c78a66bf61e96bb81d42010cf299
TgSBot <falleytrd88@mail.ru>
2023-07-06 20:49:00 +0500

    Footer Page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
-import { setupStore } from './store/Store';
+import { setupStore } from './store/Store.js';
 
 const store = setupStore();
 
